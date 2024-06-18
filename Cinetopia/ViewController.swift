9e1773d4116ca7aaf4d8843570525f0d0030653f
LucasGatica <lucas.gatica79@gmail.com>
2024-06-18 16:21:59 -0300

    Initial Commit

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by lucas gatica on 18/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
